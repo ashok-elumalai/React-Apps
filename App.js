dbeadbc8135712f08bf43faa09d262530f1333ca
Ashok Elumalai <ashok.elumalai@ayphen.com>
2025-09-29 21:45:23 +1000

    Episode-02:add react to the project and add script to start and build our App

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello Ashok, you're lucky!"
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /**
  * ReactElement is an object that converts to HTML -> that browser understands
  * ReactElement(Object) => HTML(browser understands)
  */
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm child1 H1 tag"),
-    React.createElement("h2", {}, "I'm child1 H2 tag"),
+  React.createElement("div", { id: "child1", key: "child-1" }, [
+    React.createElement("h1", { key: "ch1-11" }, "I'm child1 H1 tag"),
+    React.createElement("h2", { key: "ch1-12" }, "I'm child1 H2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm child2 H1 tag"),
-    React.createElement("h2", {}, "I'm child2 H2 tag"),
+  React.createElement("div", { id: "child2", key: "child-2" }, [
+    React.createElement("h1", { key: "ch1-21" }, "I'm child2 H1 tag"),
+    React.createElement("h2", { key: "ch1-22" }, "I'm child2 H2 tag"),
   ]),
 ]);
 console.log(parent);
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # React-Apps
+
 Creating multiple big react apps. application like youtube, Netflix etc.
+
+# Parcel (https://parceljs.org/docs/)
+
+- Dev Build
+- local server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistennt Hashing
+- Code Splitting
+- Differential Bundling - written in C++ - to support older browsers - gives different bundle for different browser/apps
+- Diagnostic
+- Error Handling
+- HTTPS - parcel provide feature that you can test in ssl/https in development in your localhost even if you have not configured to https yet
+- Tree Shaking - remove unused code
+- Different development and production/prod bundles
